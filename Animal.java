0ebac77c9379f2afe6024ba947f456203ac79875
YashPauranik1 <158431530+YashPauranik1@users.noreply.github.com>
2024-05-21 20:01:43 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,26 @@
+package Inheritance;
+
+  public class Animal extends Dogs
+{
+	void Run()
+	{
+		System.out.println("Animal is raning");
+	}
+	
+}
+ class Dogs 
+{
+	void Eat()
+	{
+		System.out.println("Dog is Eating");
+	}
+	public static void main(String[] args) 
+	{
+		Animal da=new Animal();
+		da.Run();
+		da.Eat();
+		
+		
+	}
+	
+}
