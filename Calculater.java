9df605927adc16d7312b25c3f5e9e827102cb0c5
YashPauranik1 <158431530+YashPauranik1@users.noreply.github.com>
2024-05-22 07:48:09 +0530

    Add files via upload

---
+++ Calculater.java
@@ -0,0 +1,17 @@
+package MultiLabel_Inheritance;
+
+public class Calculater 
+{
+	int Add(int a,int b)
+	{
+		int c=a+b;
+		return c;
+		
+	}
+	int Sub(int a,int b)
+	{
+		int c=a-b;
+		return c;
+	}
+
+}
