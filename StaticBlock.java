9916334429bb6c246d4b273cd78ee271a45872cd
YashPauranik1 <158431530+YashPauranik1@users.noreply.github.com>
2024-05-23 20:34:29 +0530

    Add files via upload

---
+++ StaticBlock.java
@@ -0,0 +1,13 @@
+package Day2;
+
+public class StaticBlock {
+	public static void main(String[] args) {
+		System.out.println("Main Method Executed");
+	}
+	 static
+	 {
+		 System.out.println("Static Block Executed");
+		 
+	 }  
+
+}
